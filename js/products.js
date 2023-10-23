5cf17074b34e07fdbd9bfd5fe016cac6bb0337ff
siskoweb <sisko4dev@gmail.com>
2023-10-23 11:58:33 +0100

    createing dyinamic product list

--- js/products.js
+++ js/products.js
@@ -1,6 +1,10 @@
 // Function to create a single review element
 function createReviewElement(products) {
-const section = document.querySelector(".list-products")
+    const section = document.querySelector(".list-products")
+
+    // create  product card
+    const cardProduct = document.createElement("div")
+    cardProduct.classList.add("product-card");
 
 
 }
